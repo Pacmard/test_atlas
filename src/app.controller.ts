8b140172cfac7d2e6620223528ee35033931ef1b
Ivan <030704@mail.ru>
2021-06-30 22:40:53 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,10 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/getBusInfo/:id')
+  @Get('/getBusInfo/:id/:date')
   public async deleteItemBids(
     @Param('id') busId: number,
+    @Param('date') date: Date,
   ): Promise<any> {
-    return this.appService.getBusInfo(busId);
+    return this.appService.getBusInfo(busId, date);
   }
 }
